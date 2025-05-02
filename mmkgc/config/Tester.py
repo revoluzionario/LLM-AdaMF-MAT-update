f4c0e6430f99d1d249036c803daf7b0001ba3ba1
Le <21020322@vnu.edu.vn>
2025-05-02 19:10:45 +0700

    revert

--- mmkgc/config/Tester.py
+++ mmkgc/config/Tester.py
@@ -75,12 +75,6 @@ class Tester(object):
             
 
     def run_link_prediction(self, type_constrain=False):
-        for mod_name, module in self.model.named_modules():
-        # collect all device names for parameters in this module
-            devs = {p.device for p in module.parameters()}
-            if devs:
-                print(f"{mod_name:40s} → {sorted(str(d) for d in devs)}")
-
         self.lib.initTest()
         self.data_loader.set_sampling_mode('link')
         if type_constrain:
